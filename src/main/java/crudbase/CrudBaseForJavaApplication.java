8a8fb04a2a91fec43bb78102b9e893f2ed33d135
Alysson Oliveira <3oalysson@gmail.com>
2022-07-05 18:51:25 -0300

    Fork no repositorio

--- src/main/java/crudbase/CrudBaseForJavaApplication.java
+++ src/main/java/crudbase/CrudBaseForJavaApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudBaseForJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudBaseForJavaApplication.class, args);		
+
+		SpringApplication.run(CrudBaseForJavaApplication.class, args);
 	}
 
 }
